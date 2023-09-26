93e4980afbc40a374e146ce814dba0fd3d724b53
Yugpatel6112 <98756692+Yugpatel6112@users.noreply.github.com>
2023-09-26 19:36:43 +0530

    pr_7

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MAD_Pra-7_111"
+include(":app")
